9360e2e78e4c1dcfd72a347159d67da2be689b62
LW068 <ricardocoronaberron@gmail.com>
2024-01-24 20:40:27 -0600

    [Added]: policy gradient task 2 (works on collab)

--- reinforcement_learning/policy_gradients/policy_gradient.py
+++ reinforcement_learning/policy_gradients/policy_gradient.py
@@ -14,3 +14,27 @@ def policy(matrix, weight):
     z = np.dot(matrix, weight)
     exp = np.exp(z)
     return exp / np.sum(exp, axis=1, keepdims=True)
+
+
+def policy_gradient(state, weight):
+    """
+    Compute the Monte-Carlo policy gradient.
+    
+    :param state: matrix representing the current observation of the environment
+    :param weight: matrix of random weight
+    :return: the action and the gradient (in this order)
+    """
+    probs = policy(state, weight)
+    action = np.random.choice(len(probs[0]), p=probs[0])
+
+    # constructing the softmax gradient
+    softmax_grad = np.diag(probs.ravel()) - np.outer(probs, probs)
+    selected_action_grad = softmax_grad[action]
+
+    # computing the gradient of the log probability
+    log_prob_grad = selected_action_grad / probs[0, action]
+
+    # Fginal gradient with respect to weights
+    gradient = np.dot(state.T, log_prob_grad.reshape(1, -1))
+
+    return action, gradient
