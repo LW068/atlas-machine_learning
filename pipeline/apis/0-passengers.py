d57ad55f76af633f44894e209039014305d5079c
LW068 <ricardocoronaberron@gmail.com>
2024-02-09 15:49:27 -0600

    [Fixed]: pycodestyle on file 0

--- pipeline/apis/0-passengers.py
+++ pipeline/apis/0-passengers.py
@@ -1,24 +1,26 @@
 #!/usr/bin/env python3
 """...passengers..."""
+import requests
+
 
 def availableShips(passengerCount):
     """availableShips"""
     ships = []
     next_page = 'https://swapi.dev/api/starships/'
-    
+
     while next_page is not None:
         response = requests.get(next_page)
         if response.status_code == 200:
             result = response.json()
             for ship in result['results']:
-                passengers = ship['passengers'].replace(',', '').lower()
-                # so wed need to handle cases where passengers is 'unknown'
-                if passengers not in ('unknown', 'n/a') and passengers.isdigit():
-                    if int(passengers) >= passengerCount:
+                psngrs = ship['passengers'].replace(',', '').lower()
+                # Check if passenger count is a digit and greater or equal
+                if psngrs not in ('unknown', 'n/a') and psngrs.isdigit():
+                    if int(psngrs) >= passengerCount:
                         ships.append(ship['name'])
-            next_page = result['next']  # A URL for the next page, or None if no more pages
+            next_page = result['next']
         else:
             # If the API call was not successful then we break the loop
             break
-    
+
     return ships
