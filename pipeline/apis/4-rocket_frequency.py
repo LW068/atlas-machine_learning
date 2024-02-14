cce1eb593237862c36498f34c0ec29d9a588ac23
LW068 <ricardocoronaberron@gmail.com>
2024-02-14 16:03:17 -0600

    [Fixed]: v3 to v4 of API

--- pipeline/apis/4-rocket_frequency.py
+++ pipeline/apis/4-rocket_frequency.py
@@ -6,24 +6,37 @@ import requests
 
 def get_launches_by_rocket():
     """script for rocket launch frwuency"""
-    url = 'https://api.spacexdata.com/v3/launches'
-    response = requests.get(url)
-    if response.status_code != 200:
-        print("Failed to retrieve data")
+    launches_url = 'https://api.spacexdata.com/v4/launches'
+    rockets_url = 'https://api.spacexdata.com/v4/rockets'
+    launches_response = requests.get(launches_url)
+
+    if launches_response.status_code != 200:
+        print("Failed to retrieve launches data.")
         return
 
-    launches_data = response.json()
-    rocket_launches = {}
+    rocket_id_to_name = {}
+    rocket_launch_counts = {}
 
-    for launch in launches_data:
-        rocket_name = launch['rocket']['rocket_name']
-        if rocket_name in rocket_launches:
-            rocket_launches[rocket_name] += 1
+    for launch in launches_response.json():
+        rocket_id = launch['rocket']
+        if rocket_id not in rocket_id_to_name:
+            rocket_response = requests.get(rockets_url + '/' + rocket_id)
+            if rocket_response.status_code == 200:
+                rocket_info = rocket_response.json()
+                rocket_name = rocket_info['name']
+                rocket_id_to_name[rocket_id] = rocket_name
+                if rocket_name in rocket_launch_counts:
+                    rocket_launch_counts[rocket_name] += 1
+                else:
+                    rocket_launch_counts[rocket_name] = 1
+            else:
+                print("Failed to retrieve rocket data for ID " + rocket_id)
         else:
-            rocket_launches[rocket_name] = 1
+            rocket_name = rocket_id_to_name[rocket_id]
+            rocket_launch_counts[rocket_name] = \
+                rocket_launch_counts.get(rocket_name, 0) + 1
 
-    # sortign by number of launches descending, then alphabetically
-    sorted_rockets = sorted(rocket_launches.items(),
+    sorted_rockets = sorted(rocket_launch_counts.items(),
                             key=lambda x: (-x[1], x[0]))
 
     for rocket in sorted_rockets:
