ffaba600eeec9e43e41f385deff4a7f2b9a24855
LW068 <ricardocoronaberron@gmail.com>
2024-02-26 20:28:27 -0600

    [Fix]: 6/7

--- pipeline/databases/7-max_state.sql
+++ pipeline/databases/7-max_state.sql
@@ -1,5 +1,5 @@
 -- Displays the max temperature of each state ordered by State name
-SELECT state, MAX(temperature) AS max_temp
+SELECT state, MAX(temp) AS max_temp
 FROM temperatures
 GROUP BY state
 ORDER BY state;
