682c2c90680f004a938ab7cf1a48f3472ceaa61d
LW068 <ricardocoronaberron@gmail.com>
2024-02-26 20:59:17 -0600

    [Fixing]: a couple ones

--- pipeline/databases/16-glam_rock.sql
+++ pipeline/databases/16-glam_rock.sql
@@ -1,5 +1,5 @@
 -- Lists all bands with Glam rock as their main style, ranked by their longevity
-SELECT name AS band_name, (IF(split = '0', 2020, split) - formed) AS lifespan
+SELECT name AS band_name, IF(split='-', 2020 - formed, split - formed) AS lifespan
 FROM metal_bands
-WHERE style LIKE '%Glam rock%'
+WHERE main_style = 'Glam Rock'
 ORDER BY lifespan DESC;
--- pipeline/databases/20-average_score.sql
+++ pipeline/databases/20-average_score.sql
@@ -2,8 +2,12 @@
 DELIMITER $$
 CREATE PROCEDURE ComputeAverageScoreForUser(IN user_id INT)
 BEGIN
-  UPDATE users
-  SET average_score = (SELECT AVG(score) FROM corrections WHERE user_id = users.id)
-  WHERE id = user_id;
+  DECLARE avg_score FLOAT;
+  
+  SELECT AVG(score) INTO avg_score 
+  FROM corrections 
+  WHERE user_id = user_id;
+  
+  UPDATE users SET average_score = avg_score WHERE id = user_id;
 END$$
 DELIMITER ;
