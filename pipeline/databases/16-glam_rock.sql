934f294e277f970fd54e75604e03ef84cf72aa29
LW068 <ricardocoronaberron@gmail.com>
2024-02-28 14:45:02 -0600

    [Fixed]: glma rock

--- pipeline/databases/16-glam_rock.sql
+++ pipeline/databases/16-glam_rock.sql
@@ -1,5 +1,5 @@
 -- Lists all bands with Glam rock as their main style, ranked by their longevity
-SELECT name AS band_name, IF(split='-', 2020 - formed, split - formed) AS lifespan
+SELECT name AS band_name, IFNULL((split-formed), (2020 - formed)) AS lifespan
 FROM metal_bands
-WHERE main_style = 'Glam Rock'
+WHERE style LIKE = '%Glam rock%'
 ORDER BY lifespan DESC;
