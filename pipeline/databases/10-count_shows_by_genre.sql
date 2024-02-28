53cb239979d3cd0938e222f62673fec21ad0b60e
LW068 <ricardocoronaberron@gmail.com>
2024-02-28 14:56:34 -0600

    [Fixed]: 10-11, same thing n format

--- pipeline/databases/10-count_shows_by_genre.sql
+++ pipeline/databases/10-count_shows_by_genre.sql
@@ -1,8 +1,7 @@
 -- Lists all genres and the number of shows linked to each, sorted by the number of shows linked
-SELECT genres.name AS genre, COUNT(tv_show_genres.show_id) AS number_of_shows
-FROM genres
-JOIN tv_show_genres ON genres.id = tv_show_genres.genre_id
-JOIN tv_shows ON tv_show_genres.show_id = tv_shows.id
-GROUP BY genres.name
-HAVING number_of_shows > 0
-ORDER BY number_of_shows DESC;
+SELECT tv_genres.name 'genre', COUNT(tv_show_genres.genre_id) 'number_of_shows'
+FROM tv_genres
+INNER JOIN tv_show_genres
+    ON tv_genres.id = tv_show_genres.genre_id
+GROUP BY tv_genres.name
+ORDER BY COUNT(tv_show_genres.genre_id) DESC
